45be126785b96d34e68f0de7d02406d1c35d71b2
zacp <57464940+zacpr@users.noreply.github.com>
2024-10-07 18:20:47 +1100

    ccchanges

---
+++ streamdocktools/GlobalSettings.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using StreamTimer.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streamdocktools
+{
+    public class GlobalSettings
+    {
+        [JsonProperty(PropertyName = "timers")]
+        public Dictionary<string, TimerStatus> DicTimers { get; set; }
+    }
+}
---
+++ streamdocktools/Wrappers/TimerStatus.cs
@@ -0,0 +1,40 @@
+﻿using StreamTimer.Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streamdocktools.Wrappers
+{
+    public class TimerStatus
+    {
+        public DateTime EndTime { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public string Filename { get; set; }
+
+        public string FileTitlePrefix { get; set; }
+
+        public string FileCountdownEndText { get; set; }
+
+        public bool ClearFileOnReset { get; set; }
+
+        public long PausedTimeLeft { get; set; }
+
+        public string TimeFormat { get; set; }
+
+        public TimerStatus()
+        {
+            EndTime = DateTime.Now;
+            IsEnabled = false;
+            Filename = null;
+            FileTitlePrefix = null;
+            FileCountdownEndText = null;
+            ClearFileOnReset = false;
+            PausedTimeLeft = 0;
+            TimeFormat = HelperUtils.DEFAULT_TIME_FORMAT;
+        }
+    }
+}
